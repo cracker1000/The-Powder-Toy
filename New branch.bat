513c7fd8f2919c465d8b53c4eea5b131e763657c
cracker1000 <ashishpurohit271@gmail.com>
2020-11-25 21:41:29 +0530

    Test

--- New branch.bat
+++ New branch.bat
@@ -2,4 +2,4 @@ git remote add upstream https://github.com/The-Powder-Toy/The-Powder-Toy
 git fetch upstream
 git checkout upstream/master
 git checkout -b vanilla
-git checkout -b projectile
+git checkout -b Projectile
