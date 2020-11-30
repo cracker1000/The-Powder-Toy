acc6fa2761a1609811cb38d334ecb7a43f4be4a5
jacob1 <jfu614@gmail.com>
2020-11-30 13:45:06 -0500

    Changes to "fromNewerVersion" code
    Standardizes the code to run on all mods and betas, not just snapshots and debug builds
    Ensures mods and betas can load their own saves
    Blocks publishing saves from all non-release versions if they use features not present in the previous release (currently only GoL elements)

--- src/client/GameSave.cpp
+++ src/client/GameSave.cpp
@@ -819,17 +819,16 @@ void GameSave::readOPS(char * data, int dataLength)
 							minor = bson_iterator_int(&subiter);
 					}
 				}
-	//Caution! Revert the below changes when Version 96.0 actually gets release.
-//#if defined(SNAPSHOT) || defined(DEBUG)
-				//if (major > FUTURE_SAVE_VERSION || (major == FUTURE_SAVE_VERSION && minor > FUTURE_MINOR_VERSION))
-//#else
-				//if (major > SAVE_VERSION || (major == SAVE_VERSION && minor > MINOR_VERSION))
-//#endif
-				//{
-					//String errorMessage = String::Build("Save from a newer version: Requires version ", major, ".", minor);
-					//throw ParseException(ParseException::WrongVersion, errorMessage);
-				//}
-#if defined(SNAPSHOT) || defined(DEBUG)
+#if defined(SNAPSHOT) || defined(BETA) || defined(DEBUG) || MOD_ID > 0
+				if (major > FUTURE_SAVE_VERSION || (major == FUTURE_SAVE_VERSION && minor > FUTURE_MINOR_VERSION))
+#else
+				if (major > SAVE_VERSION || (major == SAVE_VERSION && minor > MINOR_VERSION))
+#endif
+				{
+					String errorMessage = String::Build("Save from a newer version: Requires version ", major, ".", minor);
+					throw ParseException(ParseException::WrongVersion, errorMessage);
+				}
+#if defined(SNAPSHOT) || defined(BETA) || defined(DEBUG) || MOD_ID > 0
 				else if (major > SAVE_VERSION || (major == SAVE_VERSION && minor > MINOR_VERSION))
 					fakeNewerVersion = true;
 #endif
@@ -2497,6 +2496,12 @@ char * GameSave::serialiseOPS(unsigned int & dataLength)
 			}
 		}
 	}
+
+#if defined(SNAPSHOT) || defined(BETA) || defined(DEBUG) || MOD_ID > 0
+	// Mark save as incompatible with latest release
+	if (minimumMajorVersion > SAVE_VERSION || (minimumMajorVersion == SAVE_VERSION && minimumMinorVersion > MINOR_VERSION))
+		fromNewerVersion = true;
+#endif
 
 	bson b;
 	b.data = NULL;
