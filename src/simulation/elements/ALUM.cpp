c9bf9528006e4d69818dbc87a00823eb39542b33
Rebmiami <59275598+Rebmiami@users.noreply.github.com>
2023-10-08 14:09:32 -0400

    Added ALUM (currently a clone of METL)

---
+++ src/simulation/elements/ALUM.cpp
@@ -0,0 +1,42 @@
+#include "simulation/ElementCommon.h"
+
+void Element::Element_ALUM()
+{
+	Identifier = "DEFAULT_PT_ALUM";
+	Name = "ALUM";
+	Colour = PIXPACK(0x404060);
+	MenuVisible = 1;
+	MenuSection = SC_ELEC;
+	Enabled = 1;
+
+	Advection = 0.0f;
+	AirDrag = 0.00f * CFDS;
+	AirLoss = 0.90f;
+	Loss = 0.00f;
+	Collision = 0.0f;
+	Gravity = 0.0f;
+	Diffusion = 0.00f;
+	HotAir = 0.000f	* CFDS;
+	Falldown = 0;
+
+	Flammable = 0;
+	Explosive = 0;
+	Meltable = 1;
+	Hardness = 1;
+
+	Weight = 100;
+
+	HeatConduct = 251;
+	Description = "Aluminium.";
+
+	Properties = TYPE_SOLID|PROP_CONDUCTS|PROP_LIFE_DEC|PROP_HOT_GLOW;
+
+	LowPressure = IPL;
+	LowPressureTransition = NT;
+	HighPressure = IPH;
+	HighPressureTransition = NT;
+	LowTemperature = ITL;
+	LowTemperatureTransition = NT;
+	HighTemperature = 1273.0f;
+	HighTemperatureTransition = PT_LAVA;
+}
