a4d37ce26a698135949835ec13035952e795ea10
Cracker1000 <39940063+cracker1000@users.noreply.github.com>
2023-12-05 23:04:38 +0530

    Update version

--- src/lua/luascripts/crackerk.lua
+++ src/lua/luascripts/crackerk.lua
@@ -1,5 +1,5 @@
 --Cracker1000 mod interface script--
-local crackversion = 57.2 --Next version: 57.3
+local crackversion = 57.5 --Next version: 58.0
 local motw = "."
 local specialmsgval = 0
 local dr, dg, db, da, defaulttheme = 131,0,255,255, "Default"
