4efc21e8ad6177379b2f8778b3f8efe9ac056417
Cracker1000 <39940063+cracker1000@users.noreply.github.com>
2024-01-19 00:16:50 +0530

    Add missing stuff from ALUM branch

--- src/simulation/elements/SPRK.cpp
+++ src/simulation/elements/SPRK.cpp
@@ -327,6 +327,12 @@ static int update(UPDATE_FUNC_ARGS)
 						parts[ID(r)].life = 220;
 					}
 					continue;
+				case PT_ALUM:
+					if (parts[ID(r)].ctype != PT_NONE)
+					{
+						continue;
+					}
+					break;
 				}
 
 				if ((pavg == PT_INSL) || (pavg == PT_RSSS)) continue; //Insulation blocks everything past here
