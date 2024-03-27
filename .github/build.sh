5e5ba319249a8bebdcc930ed0b2d73bc6d19462d
Cracker1000 <39940063+cracker1000@users.noreply.github.com>
2024-03-27 23:20:52 +0530

    Revert "Make emscripten snapshots use starcatcher as the backend"
    This reverts commit ddff79276b1afe821c13bd481489afb4dbe9d946.

--- .github/build.sh
+++ .github/build.sh
@@ -288,13 +288,6 @@ if [[ $RELEASE_TYPE == snapshot ]] && [[ $MOD_ID != 0 ]]; then
 	>&2 echo "mods and snapshots do not mix"
 	exit 1
 fi
-if [[ $RELEASE_TYPE == snapshot ]] || [[ $MOD_ID != 0 ]]; then
-	meson_configure+=$'\t'-Dupdate_server=starcatcher.us/TPT
-	if [[ $BSH_HOST_PLATFORM == emscripten ]]; then
-		meson_configure+=$'\t'-Dserver=tptserv.starcatcher.us
-		meson_configure+=$'\t'-Dstatic_server=tptserv.starcatcher.us/Static
-	fi
-fi
 if [[ $RELEASE_TYPE != dev ]]; then
 	meson_configure+=$'\t'-Dignore_updates=false
 fi
