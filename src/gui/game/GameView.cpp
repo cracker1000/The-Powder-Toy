aa99017d18a2127e0e150ffa39359b9c92eb4cd0
Cracker1000 <39940063+cracker1000@users.noreply.github.com>
2025-10-25 20:49:52 +0530

    Fix tag button alignment

--- src/gui/game/GameView.cpp
+++ src/gui/game/GameView.cpp
@@ -281,7 +281,7 @@ GameView::GameView():
 
 	ResetVoteButtons();
 
-	tagSimulationButton = new ui::Button(ui::Point(currentX, Size.Y-16), ui::Point(WINDOWW - 402, 15), "[no tags set]", "Add simulation tags");
+	tagSimulationButton = new ui::Button(ui::Point(currentX, Size.Y-16), ui::Point(176, 15), "[no tags set]", "Add simulation tags");
 	tagSimulationButton->Appearance.HorizontalAlign = ui::Appearance::AlignLeft;
 	tagSimulationButton->SetIcon(IconTag);
 	//currentX+=252;
